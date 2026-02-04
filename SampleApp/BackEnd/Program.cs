2316f47511ced83dd6b23a3752668268a4e6ac15
Taiza-Moneye Scarlett <taizamoneye@outlook.com>
2026-02-04 01:03:50 +0000

    Connect SQL

--- SampleApp/BackEnd/Program.cs
+++ SampleApp/BackEnd/Program.cs
@@ -1,54 +1,47 @@
-using Microsoft.AspNetCore.OpenApi;
-using Scalar.AspNetCore;
+using Microsoft.EntityFrameworkCore;
+using BackEnd.Infrastructure.Data;
 
+//sets the app upp, merges configurations, and starts the web server
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi(options =>
+//registers services for dependency injection ~ data access, UI rendering, etc.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+
+
+
+builder.Services.AddDbContext<AppDbContext>(options =>
 {
-    // current workaround for port forwarding in codespaces
-    // https://github.com/dotnet/aspnetcore/issues/57332
-    options.AddDocumentTransformer((document, context, ct) =>
-    {
-        document.Servers = [];
-        return Task.CompletedTask;
-    });
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+
+
+
+//finalised the configuration and starts the app
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+//error handling and security settings
+if (!app.Environment.IsDevelopment())// if not in development environment
 {
-    app.MapOpenApi();
-    app.MapScalarApiReference();
+    app.UseExceptionHandler("/Error");//redirects to error page on exceptions
+    app.UseHsts();//enables HTTP Strict Transport Security for enhanced security
 }
 
+//middleware pipeline configuration -defines how HTTP requests are handled
 app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
+app.UseStaticFiles();
+app.UseRouting();
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
 app.Run();
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+
+
+
+
+
+
+
+
+
